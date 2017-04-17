a1fbf758f5f423524609caf5b7467f67a15df2d8
satishpeyyety <satishpeyyety@yahoo.com>
2017-04-17 21:39:24 +0530

    compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -42,6 +42,10 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
+	err:= stub.PutState("hello_world",[]byte(args[0]))
+	if err!=nil {
+	  return nil, err
+	}
 
 	return nil, nil
 }
@@ -53,20 +57,42 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub,args)
 	}
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
+func (t *SimpleChaincode)write(stub shim.ChainCodeStubInterface,args[] string) ([]byte, error) {
+	var name, value string
+	var err error
+	fmt.Println("running write")
+	
+	if len(args)!=2 {
+	 return nil,errors.New("Incorrect number of arguments.Expecting 2- name of the variable and value to set")
+	}
+	
+	name = args[0]
+	value = args[1]
+	err. stub.PutState(name,[]byte(value))
+	if err!=nil {
+		return nil, err
+	}
+	
+	return nil, nil
+}
+
+
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
 	// Handle different functions
-	if function == "dummy_query" {											//read a variable
+	if function == "read" {											//read a variable
 		fmt.Println("hi there " + function)						//error
-		return nil, nil;
+		return t.read(stub,args)
 	}
 	fmt.Println("query did not find func: " + function)						//error
 
